923f1408e2a86a699c66f46c70826c9436d948a7
cofficer <christoffer.gahnstrom@hotmail.com>
2017-08-19 23:15:56 +0200

    dont baseline pre trl avg

--- freq_average.m
+++ freq_average.m
@@ -3,7 +3,7 @@ function [avgFreq] = freq_average(cfgin)
 
 cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/trial/freq/')
 
-freqrange  = 'low';
+freqrange  = 'high';
 doplot     = 1;
 compSwitch = 0;
 freqpath   = dir(sprintf('*%s*',freqrange));
@@ -18,18 +18,18 @@ for ipart = 1:length(freqpath)
 
   load(freqpath(ipart).name)
 
-  if strcmp(freqrange,'low')
   %select trials, and average over trials
     gdtrl = freq.trialinfo(:,6)~=43;
-    switchtrl1 = freq.trialinfo(:,6)==42; %45
-    switchtrl2 = freq.trialinfo(:,6)==45; %45
-    noswitchtrl1 = freq.trialinfo(:,6)==41; %45
-    noswitchtrl2 = freq.trialinfo(:,6)==46; %45
 
-    allswitch = switchtrl1+switchtrl2;
-    allnoswch = noswitchtrl1+noswitchtrl2;
-
+    %If comparing perceptual switch with no switch.
     if compSwitch
+      switchtrl1 = freq.trialinfo(:,6)==42; %45
+      switchtrl2 = freq.trialinfo(:,6)==45; %45
+      noswitchtrl1 = freq.trialinfo(:,6)==41; %45
+      noswitchtrl2 = freq.trialinfo(:,6)==46; %45
+
+      allswitch = switchtrl1+switchtrl2;
+      allnoswch = noswitchtrl1+noswitchtrl2;
       cfg   = [];
       cfg.trials = logical(allswitch);
       %cfg.frequency = [12 35];
@@ -46,6 +46,13 @@ for ipart = 1:length(freqpath)
       freq.powspctrm = (freqS.powspctrm-freq.powspctrm);
     else
 
+      %Probably good to baseline before averaging.
+      %Not sure anymore.
+      %cfg = [];
+      %cfg.baseline = [0.5 1];
+      %cfg.baselinetype = 'relative';
+      %freq = ft_freqbaseline(cfg,freq);
+
       cfg   = [];
       cfg.trials = gdtrl;
       %cfg.frequency = [12 35];
@@ -60,7 +67,7 @@ for ipart = 1:length(freqpath)
       avgFreq(ipart,:,:,:) = freq.powspctrm;
 
     end
-  end
+
 
 
 end
@@ -79,21 +86,36 @@ if doplot
   cfg.baseline = [0.5 1];
   cfg.baselinetype = 'relative';
   cfg.masktype     = 'saturation';
-  cfg.zlim         = [0.5 1.5];
-  cfg.ylim         = [7 12];
+  cfg.zlim         = [0.9 1.1];
+  cfg.ylim         = [36 120];
   cfg.layout       = 'CTF275_helmet.lay';
-  cfg.xlim         = [2 2.25];%[0.5 4 ];%[2.1 2.4];%
-  %cfg.channel      = freq.label(idx_occ);
+  cfg.xlim         = [0.5 4];%[2 2.25];%[0.5 4 ];%[2.1 2.4];%
+  cfg.channel      = freq.label(idx_occ);
   cfg.interactive = 'no';
-% ft_singleplotTFR(cfg,freq);
- % %ft_multiplotTFR(cfg,freq)
-  ft_topoplotTFR(cfg,freq)
+  ft_singleplotTFR(cfg,freq);
+  %ft_multiplotTFR(cfg,freq)
+  %ft_topoplotTFR(cfg,freq)
   colormap(flipud(brewermap(64,'RdBu')))
   colorbar
 end
 
 cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/trial/freq/figures')
-saveas(gca,'alphaTopo24avg.png','png')
+saveas(gca,'gammaMultiTopo24-4.png','png')
 
+%%
+% for the multiple plots also
+cfg = [];
+cfg.xlim = [0.6:0.35:4];
+cfg.ylim = [64 100];
+cfg.zlim = [0.95 1.05];
+cfg.baseline = [0.5 1];
+cfg.baselinetype = 'relative';
+cfg.masktype     = 'saturation';
+cfg.layout = 'CTF275_helmet.lay';
+cfg.comment = 'xlim';
+cfg.commentpos = 'title';
+figure;
+colormap(flipud(brewermap(64,'RdBu')))
+ft_topoplotTFR(cfg,freq);
 
 end
--- startup.m
+++ startup.m
@@ -1,5 +1,5 @@
 
-%Startup settings: 
+%Startup settings:
 
 %Set paths
 warning off
@@ -8,27 +8,27 @@ whichFieldtrip = '2017-3';%'2017-2';
 
 
 if strcmp(whichFieldtrip,'2015')
-    
+
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/code/analysis'))
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20151020/')
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20151020/qsub')
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous'))
      addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos'))
     ft_defaults
-    
+
 elseif strcmp(whichFieldtrip,'2016')
-    
+
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/code/analysis'))
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20160601/')
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20160601/qsub')
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous'))
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos'))
     ft_defaults
-    
-    
+
+
 elseif strcmp(whichFieldtrip,'2017')
 
-    
+
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/code/analysis'))
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20170124/')
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20170124/qsub')
@@ -38,7 +38,7 @@ elseif strcmp(whichFieldtrip,'2017')
 
 elseif strcmp(whichFieldtrip,'2017-2')
 
-    
+
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/code/analysis'))
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20170528/')
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20170528/qsub')
@@ -48,18 +48,17 @@ elseif strcmp(whichFieldtrip,'2017-2')
     
 elseif strcmp(whichFieldtrip,'2017-3')
 
-    
-    addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/code/analysis'))
+
+    addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/code/'))
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20170802/')
     addpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/fieldtrip-20170802/qsub')
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous'))
     addpath(genpath('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos'))
     ft_defaults
-    
-    
+
+
 end
 %Set graphs:
 set(0,'DefaultAxesColorOrder',cbrewer('qual','Set2',8))
 set(0,'DefaultLineLineWidth',1.2)
 set(0,'DefaultFigureColormap',cbrewer('div','PuOr',64))
-
