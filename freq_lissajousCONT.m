6a68f108def410293d94f9aeb32b99ed677f8c2f
cofficer <christoffer.gahnstrom@hotmail.com>
2017-09-24 20:22:41 +0200

    will run only on missing freq data

--- freq_lissajousCONT.m
+++ freq_lissajousCONT.m
@@ -1,14 +1,14 @@
 function freq = freq_lissajousCONT(cfgin)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Run time-frequency analysis for continuous data. 28/08-2017. cofficer.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%TODO: Consider the case of no button presses. Remove for now.
-%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %Run time-frequency analysis for continuous data. 28/08-2017. cofficer.
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %TODO: Consider the case of no button presses. Remove for now.
+  %%
 
-%loop and load preproc data
-%example /mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/continuous/preprocessed/P22/
-%
-try
+  %loop and load preproc data
+  %example /mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/continuous/preprocessed/P22/
+  %
+  try
 
 
 
@@ -21,131 +21,131 @@ try
 
     for iblock = 1:length(dat_name)
 
-        load(dat_name(iblock).name)
+      %Setting for spectral decomposition
+      cfg             = [];
+      cfg.output      = 'pow';
+      % cfg.output = 'fourier';
+      cfg.channel     = 'MEG';
+      cfg.keeptapers  = 'no';
+      cfg.pad         = 7;
+      cfg.method      = 'mtmconvol';
+      cfg.trigger     = 'selfoccl';
+      cfg.channel     ='MEG'; %
+      cfg.trials      = 'all';
+      cfg.freqanalysistype = 'high';
 
-        outputfile = sprintf('%sfreq_%s_%sBlock%s-26-26.mat',cfgin.restingfile(2:3),cfg.freqanalysistype,cfg.trigger,dat_name(iblock).name(1));
+      load(dat_name(iblock).name)
 
-        cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/continuous/freq/')
+      outputfile = sprintf('%sfreq_%s_%sBlock%s-26-26.mat',cfgin.restingfile(2:3),cfg.freqanalysistype,cfg.trigger,dat_name(iblock).name(1));
 
-        if exist(outputfile)==2
-          continue
-        end
+      cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/continuous/freq/')
 
-        %Seperate the data into orthogonal sensors
-        cfg_pn = [];
-        cfg_pn.method = 'distance';
-        cfg_pn.template = 'C:\Users\Thomas Meindertsma\Documents\MATLAB\CTF275_neighb.mat';
-        cfg_pn.template = 'CTF275_neighb';
-        cfg_pn.channel = 'MEG';
+      if exist(outputfile)==2
+        continue
+      end
 
-        cfg_mp.planarmethod = 'sincos';
-        cfg_mp.trials = 'all';
-        cfg_mp.channel = 'MEG';
-        cfg_mp.neighbours = ft_prepare_neighbours(cfg_pn, data);
-        data = ft_megplanar(cfg_mp, data);
+      %Seperate the data into orthogonal sensors
+      cfg_pn = [];
+      cfg_pn.method = 'distance';
+      cfg_pn.template = 'C:\Users\Thomas Meindertsma\Documents\MATLAB\CTF275_neighb.mat';
+      cfg_pn.template = 'CTF275_neighb';
+      cfg_pn.channel = 'MEG';
 
-
-        %Setting for spectral decomposition
-        cfg             = [];
-        cfg.output      = 'pow';
-        % cfg.output = 'fourier';
-        cfg.channel     = 'MEG';
-        cfg.keeptapers  = 'no';
-        cfg.pad         = 7;
-        cfg.method      = 'mtmconvol';
-        cfg.trigger     = 'selfoccl';
-        cfg.channel     ='MEG'; %
-        cfg.trials      = 'all';
-        cfg.freqanalysistype = 'high';
-
-
-        switch cfg.freqanalysistype
-            case 'high'
-                cfg.taper = 'dpss'; % high frequency-optimized analysis (smooth)
-                cfg.keeptrials  = 'yes';
-                cfg.foi = 36:2:150;
-                cfg.t_ftimwin = ones(length(cfg.foi),1) .* 0.5;
-                cfg.tapsmofrq = ones(length(cfg.foi),1) .* 8;
-            case 'low'
-                cfg.taper = 'hanning'; % low frequency-optimized analysis
-                cfg.keeptrials  = 'yes'; % needed for fourier-output
-                %           cfg.keeptapers = 'yes'; % idem
-                cfg.foi = 3:35;
-                cfg.t_ftimwin = ones(length(cfg.foi),1) .* 0.5; %400ms time window?
-                cfg.tapsmofrq = ones(length(cfg.foi),1) .* 4;
-            case 'full'
-                cfg.taper = 'dpss'; % high frequency-optimized analysis (smooth)
-                cfg.keeptrials  = 'yes';
-                cfg.foi = 0:2:150;
-                cfg.t_ftimwin = ones(length(cfg.foi),1) .* 0.5;   % length of time window = 0.4 sec
-                cfg.tapsmofrq = ones(length(cfg.foi),1) .* 8;
-
-        end
-
-
-        %Select the step sizes.
-        if strcmp(cfg.trigger,'baseline')
-
-            cfg.toi = -0.50:0.05:0;
-
-        elseif strcmp(cfg.trigger,'selfoccl')
-
-            %cfg.toi = 0.25:0.05:4.25;
-            cfg.toi = -2.5:0.05:2.5;
-
-        elseif strcmp(cfg.trigger,'resp')
-
-            cfg.toi = -0.60:0.05:0;            %still to figure
-
-        elseif strcmp(cfg.trigger,'cue')
-
-            cfg.toi = -0.5:0.05:0.5;            %still to figure
-
-        end
+      cfg_mp.planarmethod = 'sincos';
+      cfg_mp.trials = 'all';
+      cfg_mp.channel = 'MEG';
+      cfg_mp.neighbours = ft_prepare_neighbours(cfg_pn, data);
+      data = ft_megplanar(cfg_mp, data);
 
 
 
+      switch cfg.freqanalysistype
+      case 'high'
+        cfg.taper = 'dpss'; % high frequency-optimized analysis (smooth)
+        cfg.keeptrials  = 'yes';
+        cfg.foi = 36:2:150;
+        cfg.t_ftimwin = ones(length(cfg.foi),1) .* 0.5;
+        cfg.tapsmofrq = ones(length(cfg.foi),1) .* 8;
+      case 'low'
+        cfg.taper = 'hanning'; % low frequency-optimized analysis
+        cfg.keeptrials  = 'yes'; % needed for fourier-output
+        %           cfg.keeptapers = 'yes'; % idem
+        cfg.foi = 3:35;
+        cfg.t_ftimwin = ones(length(cfg.foi),1) .* 0.5; %400ms time window?
+        cfg.tapsmofrq = ones(length(cfg.foi),1) .* 4;
+      case 'full'
+        cfg.taper = 'dpss'; % high frequency-optimized analysis (smooth)
+        cfg.keeptrials  = 'yes';
+        cfg.foi = 0:2:150;
+        cfg.t_ftimwin = ones(length(cfg.foi),1) .* 0.5;   % length of time window = 0.4 sec
+        cfg.tapsmofrq = ones(length(cfg.foi),1) .* 8;
 
-        %Fieltrip fourier
-        cfg.polyremoval = -1; %Detrend that data.
-        freq = ft_freqanalysis(cfg, data);
-
-        %Combine planar
-        cfgC=[];
-        cfgC.trials='all';
-        cfgC.combinemethod='sum';
-        freq=ft_combineplanar(cfgC,freq);
-
-
-        % %save(outputfile,'freq','-v7.3');
-        % %plot TFR
-        %  cfg = [];
-        %  cfg.baseline = [0.5 1];
-        %  cfg.baselinetype = 'relchange';
-        %  cfg.masktype     = 'saturation';
-        %  cfg.zlim         = 'maxmin';
-        %  cfg.layout       = 'CTF275.lay';
-        %  cfg.xlim         = [1.25 3.25 ];
-        % % %cfg.channel      = 'MRC15';
-        %  cfg.interactive = 'yes';
-        % % figure
-        % % %ft_singleplotTFR(cfg,freq);
-        % % %ft_multiplotTFR(cfg,freq)
-        %  ft_topoplotTFR(cfg,freq)
+      end
 
 
-        cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/continuous/freq/')
-        [pathstr, name] = fileparts(cfgin.fullpath);
-        fprintf('Saving %s from...\n %s\n', name, pathstr)
+      %Select the step sizes.
+      if strcmp(cfg.trigger,'baseline')
 
-        save(outputfile, 'freq','-v7.3');
+        cfg.toi = -0.50:0.05:0;
 
-        %There might be an error due to memory
-        clear freq
+      elseif strcmp(cfg.trigger,'selfoccl')
+
+        %cfg.toi = 0.25:0.05:4.25;
+        cfg.toi = -2.5:0.05:2.5;
+
+      elseif strcmp(cfg.trigger,'resp')
+
+        cfg.toi = -0.60:0.05:0;            %still to figure
+
+      elseif strcmp(cfg.trigger,'cue')
+
+        cfg.toi = -0.5:0.05:0.5;            %still to figure
+
+      end
+
+
+
+
+      %Fieltrip fourier
+      cfg.polyremoval = -1; %Detrend that data.
+      freq = ft_freqanalysis(cfg, data);
+
+      %Combine planar
+      cfgC=[];
+      cfgC.trials='all';
+      cfgC.combinemethod='sum';
+      freq=ft_combineplanar(cfgC,freq);
+
+
+      % %save(outputfile,'freq','-v7.3');
+      % %plot TFR
+      %  cfg = [];
+      %  cfg.baseline = [0.5 1];
+      %  cfg.baselinetype = 'relchange';
+      %  cfg.masktype     = 'saturation';
+      %  cfg.zlim         = 'maxmin';
+      %  cfg.layout       = 'CTF275.lay';
+      %  cfg.xlim         = [1.25 3.25 ];
+      % % %cfg.channel      = 'MRC15';
+      %  cfg.interactive = 'yes';
+      % % figure
+      % % %ft_singleplotTFR(cfg,freq);
+      % % %ft_multiplotTFR(cfg,freq)
+      %  ft_topoplotTFR(cfg,freq)
+
+
+      cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/continuous/freq/')
+      [pathstr, name] = fileparts(cfgin.fullpath);
+      fprintf('Saving %s from...\n %s\n', name, pathstr)
+
+      save(outputfile, 'freq','-v7.3');
+
+      %There might be an error due to memory
+      clear freq
     end
 
 
-catch err
+  catch err
 
     cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/')
     fid=fopen('freqContErrors','a+');
@@ -157,6 +157,6 @@ catch err
     fclose(fid)
 
 
-end
+  end
 
 end
