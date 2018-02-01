be81b756f22ede204ce95a14b5b437de2e34ff9e
cofficer <christoffer.gahnstrom@hotmail.com>
2018-02-01 15:23:36 +0100

    ready to run averages

--- freq_average_individual_all.m
+++ freq_average_individual_all.m
@@ -39,53 +39,61 @@ function [freq,switchTrial,stableTrial]=freq_average_individual_all(cfgin)
     disp(freqpath(blocks_ID(iblocks)).name)
     load(freqpath(blocks_ID(iblocks)).name)
 
-
-    % %Remove trials with response too close to stimulus onset.
-    % idx_trl = ((freq.trialinfo(:,9)-freq.trialinfo(:,7))./1200)<1;
-    % sum(idx_trl)
-    % cfg = [];
-    % cfg.trials = idx_trl;
-    % freq = ft_selectdata(cfg,freq);
-
-
-    %Run within trial baseline
-    cfg                       = [];
-    cfg.subtractmode          = 'within_norm'; % norm_avg within_norm, within
-    %Find first nonnan timepoint in data, and use that before and after self-O
-    %What if there are no nans at all...
-    if strcmp(cfgin.blocktype,'continuous')
-      idx_nan = ~isnan(freq.powspctrm(1,1,1,:));
-      % idx_time=find(diff(idx_nan)==-1);
-      % switchTrial.time(idx_time)
-      cfg.baselinewindow        = [freq.time(1) freq.time(end)];
-
+    %Append all the data
+    if iblocks>1
+      [freqAll] = append_trialfreq([],freqAll,freq);
     else
-      %4.5=stim_off.
-      if strcmp(cfgin.baseline,'cue')
-        cfg.baselinewindow=[2.35 2.85];
-      else
-        cfg.baselinewindow        = [cfgin.baseline(1) cfgin.baseline(2)]; %-0.4 -0.1
-      end
+      freqAll=freq;
     end
-
-    [freq_base] = baseline_lissajous_all(freq,cfg,cfgin);
-
-
-    %Make the freq the trial average
-    cfg =[];
-    cfg.avgoverrpt = 'yes';
-    freq = ft_selectdata(cfg,freq);
-
-    %substitute powspctrm with own baselined data
-    freq.powspctrm=freq_base;
-
-    %Save the freq in new folder
-    d_average = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/%s/freq/average/resp',cfgin.blocktype);
-    cd(d_average)
-    freqtosave = sprintf('freqavgs_all_%s_%d',cfgin.freqrange,cfgin.part_ID);
-    save(freqtosave,'freq')
-
-
   end
 
+  % %Remove trials with response too close to stimulus onset.
+  % idx_trl = ((freq.trialinfo(:,9)-freq.trialinfo(:,7))./1200)<1;
+  % sum(idx_trl)
+  % cfg = [];
+  % cfg.trials = idx_trl;
+  % freq = ft_selectdata(cfg,freq);
+
+
+  %Run within trial baseline
+  cfg                       = [];
+  cfg.subtractmode          = 'within_norm'; % norm_avg within_norm, within
+  %Find first nonnan timepoint in data, and use that before and after self-O
+  %What if there are no nans at all...
+  if strcmp(cfgin.blocktype,'continuous')
+    idx_nan = ~isnan(freq.powspctrm(1,1,1,:));
+    % idx_time=find(diff(idx_nan)==-1);
+    % switchTrial.time(idx_time)
+    cfg.baselinewindow        = [freq.time(1) freq.time(end)];
+
+  else
+    %4.5=stim_off.
+    if strcmp(cfgin.baseline,'cue')
+      cfg.baselinewindow=[2.35 2.85];
+    else
+      cfg.baselinewindow        = [cfgin.baseline(1) cfgin.baseline(2)]; %-0.4 -0.1
+    end
+  end
+
+  [freq_base] = baseline_lissajous_all(freqAll,cfg,cfgin);
+
+
+  %Append data, by taking the average
+  %Make the freq the trial average
+  cfg =[];
+  cfg.avgoverrpt = 'yes';
+  freq = ft_selectdata(cfg,freq);
+
+  %substitute powspctrm with own baselined data
+  freq.powspctrm=freq_base;
+
+
+
+
+
+  %Save the freq in new folder
+  d_average = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/%s/freq/average/resp',cfgin.blocktype);
+  cd(d_average)
+  freqtosave = sprintf('freqavgs_all_%s_%d',cfgin.freqrange,cfgin.part_ID);
+  save(freqtosave,'freq')
 end
--- main_parallel_lissajous.m
+++ main_parallel_lissajous.m
@@ -37,7 +37,7 @@ for icfg = 1:length(restingpaths)
 end
 
 %Define script to run and whether to run on the torque
-runcfg.execute         = 'freq_plot'; %freq preproc, parallel, findsquid, check_nSensors,freq_plot
+runcfg.execute         = 'freq'; %freq preproc, parallel, findsquid, check_nSensors,freq_plot
 runcfg.timreq          = 2000;      %number of minutes.
 runcfg.parallel        = 'torque';  %local or torque
 
@@ -110,7 +110,7 @@ switch runcfg.execute
         cfgin{icfgin}.topo_tfr = 'no_plot'; %topo-all, no_plot
         %This depends on the what the data is locked to.
         %If baseline cue then load the precue data as basline.
-        cfgin{icfgin}.baseline                = 'cue'; %[-2.75 -2.25];
+        cfgin{icfgin}.baseline                = 'resp'; %[-2.75 -2.25];
       end
 
 
