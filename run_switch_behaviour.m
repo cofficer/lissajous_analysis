a6254044f436332b90380da48314d80732de9871
Christoffer Gahnstrohm <c.gahnstrohm@spiers07.local>
2018-11-28 15:54:05 +0000

    binned meg cluster save

--- run_switch_behaviour.m
+++ run_switch_behaviour.m
@@ -10,7 +10,8 @@ function output = run_switch_behaviour(cfgin)
   % Get the stat.mask
   % load('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/continuous/freq/figures/2018-06-04_statistics_switchvsnoswitch.mat')
   % Get the most recent stat.mask and plot it to check....
-  load('/Users/c.gahnstrohm/Dropbox/PhD/Lissajous/results_plots/2018-11-17_statistics_switchvsnoswitch.mat')
+  % load('/Users/c.gahnstrohm/Dropbox/PhD/Lissajous/results_plots/2018-11-17_statistics_switchvsnoswitch.mat')
+  load('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/2018-11-17_statistics_switchvsnoswitch.mat')
 
   % Load to get one freq.
   % load('/Users/c.gahnstrohm/Dropbox/PhD/Lissajous/both_self_freqavg/freqavgs_switch_continuous_low_7.mat')
@@ -38,9 +39,14 @@ function output = run_switch_behaviour(cfgin)
   nFiles=1;
   meg_cluster_all=[];
 
-  for ifile = 1:nFiles
 
-    load('/Users/c.gahnstrohm/Dropbox/PhD/Lissajous/continous_self_freq/06freq_low_selfocclBlock2.mat')
+  cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/Lissajous/continuous/freq/self')
+  nFiles = dir('*low*');
+
+  for ifile = 1:length(nFiles)
+
+    % Needs to load every relevant file...
+    load(nFiles(ifile).name)
 
     %Always 272 self-occlusions:
     %Step 1. Figure out which are the switches.
@@ -62,12 +68,29 @@ function output = run_switch_behaviour(cfgin)
     cluster_mask=~isnan(data_comp);
     meg_cluster=correlate_stay_duration_switches(freq,cluster_mask);
 
-    meg_cluster_all=[meg_cluster_all;meg_cluster];
-
+    meg_cluster_all=horzcat(meg_cluster_all,meg_cluster);
+    size(meg_cluster_all)
   end
 
   % Save the meg_cluster_all matrix, one per participant.
+  cd('/home/chrisgahn/Documents/MATLAB/Lissajous/continuous/cluster_freq')
 
+  filename=sprintf('%s_meg_cluster.mat',cfgin.restingfile)
+  save('meg_cluster_all.mat','meg_cluster_all')
 
+  figure(1),clf
+  stay_durs   = meg_cluster_all(2,:);
+  meg_act     = meg_cluster_all(1,:);
+  stay_durs   = stay_durs(meg_act<1e-19);
+  meg_act     = meg_act(meg_act<1e-19);
+  meg_act(isnan(stay_durs))=[];
+  stay_durs(isnan(stay_durs))=[];
+
+  scatter(stay_durs,meg_act,'*')
+  xlabel('upcoming n of self-occlusions')
+  ylabel('switch-cluster sum activity')
+  saveas(gca,'scatter_meg_cluster.png','png')
+
+  [aa,bb]=corr(stay_durs',meg_act')
 
 end
